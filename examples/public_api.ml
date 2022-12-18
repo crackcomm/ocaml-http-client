df531ef55f2d1a270778b81b1923678bef4491aa
Łukasz Kurowski <crackcomm@gmail.com>
2022-12-18 12:18:20 +0100

    chore: update examples and dependencies

--- examples/public_api.ml
+++ examples/public_api.ml
@@ -4,46 +4,45 @@ open Core
 open Async
 open Http_api
 
-module Error_message = struct
-  type t = Api_t.response_message
+module Error_message = Response.Make (struct
+  type t = Http_api_message.t [@@deriving sexp_of]
 
-  let of_response (res : t) =
-    { Route.Error_message.code = res.code; message = res.message }
+  let of_string s =
+    let res = Api_j.response_message_of_string s in
+    { Http_api_message.code = res.code; message = res.message }
   ;;
-
-  let of_string s = of_response (Api_j.response_message_of_string s)
-end
+end)
 
 module Exchange_info = Route.Make (struct
   let method_name = "bnc_perp.Public_api.Exchange_info"
 
-  module Request = struct
-    type t = unit
+  module Request = Request.Make_get (struct
+    type t = unit [@@deriving sexp_of]
 
-    let path = "/fapi/v1/exchangeInfo"
-    let method_ = `GET
-    let weight = Route.Weight.Static 1
+    let path _ = "/fapi/v1/exchangeInfo"
+    let weight _ = 1
     let query _ = None
-  end
+  end)
 
-  module Response = Route.Make_response_on_success (struct
-    type t = Api_t.exchange_info
+  module Response = Response.Make_on_success (struct
+    include Response.Make (struct
+      type t = Api_t.exchange_info [@@deriving sexp_of]
 
-    let of_string = Api_j.exchange_info_of_string
+      let of_string = Api_j.exchange_info_of_string
+    end)
 
-    module Error_message = Error_message
+    module Response_error = Error_message
   end)
 end)
 
 let%expect_test "Public Binance Perp API" =
   let pool =
-    Http_client.Pool.of_host
-      ~size:3
-      ~burst_per_second:10
-      ~sustained_rate_per_sec:5.0
-      "fapi.binance.com"
+    Http_client.create_test
+      (module Cohttp_backend.Client)
+      (Uri.make ~scheme:"https" ~host:"fapi.binance.com" ())
   in
-  Exchange_info.fetch_exn pool ()
+  Exchange_info.call_exn pool ()
+  >>| Http_api_message.Result.value_exn ~here:[%here]
   >>| fun info ->
   List.length info.symbols > 150 |> Bool.to_string |> print_endline;
   [%expect {| true |}];
--- examples/public_api.mli
+++ examples/public_api.mli
@@ -1,4 +1,6 @@
 (* SPDX-License-Identifier: BSD-3-Clause *)
 
 module Exchange_info :
-  Http_api.Route.S with type Request.t := unit and type Response.t := Api_t.exchange_info
+  Http_api.Route.S
+    with type Request.t := unit
+     and type Response.t := Api_t.exchange_info Http_api_message.Result.t
